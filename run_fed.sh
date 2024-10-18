258a41884264e1d11c66fe5513252e1ad2be9edd
biandeshi <1345424029@qq.com>
2024-10-18 13:43:52 +0800

    update federated method

---
+++ run_fed.sh
@@ -0,0 +1,14 @@
+PD=data/douban
+PREFIX1=online
+PREFIX2=offline
+TRAINRATIO=0.2
+
+python Fed.py \
+--s_edge ${PD}/${PREFIX1}/raw/edgelist \
+--t_edge ${PD}/${PREFIX2}/raw/edgelist \
+--gt_path ${PD}/anchor/node,split=${TRAINRATIO}.test.dict \
+--train_path ${PD}/anchor/node,split=${TRAINRATIO}.train.dict \
+--out_path ${PD}/embeddings \
+--dim 128 \
+--lr 0.001 \
+--epochs 10
