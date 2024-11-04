c93666ca8ce1a8862c2f6eba1b10c3558438af80
biandeshi <1345424029@qq.com>
2024-11-04 20:11:08 +0800

    Add some pics output

--- pics.py
+++ pics.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
 
-epoch = [1, 2, 3, 4, 5]
-accuracy = [0.8, 0.9, 0.7, 0.6, 0.5]
+ratio = [i/10 for i in range(1, 10)]
+p10 = [0.4945, 0.6525, 0.7663, 0.8137, 0.8605, 0.8772, 0.9137, 0.9286, 0.9286]
+mrr = [0.2429, 0.3489, 0.4715, 0.5330, 0.5741, 0.5957, 0.6217, 0.6405, 0.6425]
 
-plt.plot(epoch, accuracy)
+plt.plot(ratio, p10)
+plt.xlabel('ratio')
+plt.ylabel('precision@10')
 plt.grid(True)
-plt.savefig('plot.png')
+plt.savefig('pics/ratio_p10.png')
+plt.cla()
+
+plt.plot(ratio, mrr)
+plt.xlabel('ratio')
+plt.ylabel('MRR')
+plt.grid(True)
+plt.savefig('pics/ratio_MRR.png')
--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ PREFIX2=offline
 EPOCHS=1000
 ALPHA=1.0
 LR=0.001
-ALPHA=1.0
 
 for TRAINRATIO in $(seq 0.1 0.1 0.9);do
     # for LR in $(seq 0.001 0.001 0.01);do
@@ -19,7 +18,25 @@ for TRAINRATIO in $(seq 0.1 0.1 0.9);do
             --dim 128 \
             --lr ${LR} \
             --epochs ${EPOCHS} \
-            --alpha ${ALPHA} > output/${DATASET}/${PREFIX1}_${PREFIX2}_tr=${TRAINRATIO}_lr=${LR}_epochs=${EPOCHS}_alpha=${ALPHA}.txt
+            --alpha ${ALPHA} > output/${DATASET}/FedWoNeg_${PREFIX1}_${PREFIX2}_tr=${TRAINRATIO}_lr=${LR}_epochs=${EPOCHS}_alpha=${ALPHA}.txt
     #     done
     # done
+done
+
+DATASET=twitter_foursquare
+PD=data/${DATASET}
+PREFIX1=twitter
+PREFIX2=foursquare
+
+for TRAINRATIO in $(seq 0.1 0.1 0.9);do
+    python FedWoNeg.py \
+    --s_edge ${PD}/${PREFIX1}/raw/edgelist \
+    --t_edge ${PD}/${PREFIX2}/raw/edgelist \
+    --gt_path ${PD}/anchor/node,split=${TRAINRATIO}.test.dict \
+    --train_path ${PD}/anchor/node,split=${TRAINRATIO}.train.dict \
+    --out_path ${PD}/embeddings \
+    --dim 128 \
+    --lr ${LR} \
+    --epochs ${EPOCHS} \
+    --alpha ${ALPHA} > output/${DATASET}/FedWoNeg_${PREFIX1}_${PREFIX2}_tr=${TRAINRATIO}_lr=${LR}_epochs=${EPOCHS}_alpha=${ALPHA}.txt
 done
